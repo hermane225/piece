9709dd9a37997c693fc349c6da92216f34696f74
hermane225 <mj2190175@gmail.com>
2026-05-06 12:05:44 +0000

    update

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,11 +1,10 @@
 import "dotenv/config";
+import { defineConfig } from "prisma/config";
 
 const datasourceUrl = process.env.DATABASE_URL;
 if (!datasourceUrl) {
   throw new Error("DATABASE_URL environment variable is not set");
 }
-
-import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
